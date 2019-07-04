557389850a18e81cc4435aa96c72f650f6d5e003
jacampb <jacampbell@protonmail.com>
2019-07-04 17:33:16 -0400

    fixed condition on GetGPA

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -128,7 +128,7 @@ namespace GradeBook.GradeBooks
                     gpa = 0;
                     break;
             }
-            if (IsWeighted = true && (studentType.ToString() == "Honors" || studentType.ToString() == "DualEnrolled"))
+            if (IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
             {
                 gpa++;
             }
